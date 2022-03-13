db61c693e1ae5259ba576e826b1567606d8de628
Cristiane Dias <cristianedias@Cris-MAC.local>
2022-03-13 16:30:38 -0400

    PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -1,11 +1,24 @@
-{
-	"folders": [
-		{
-			"path": "."
-		},
-		{
-			"path": "../Resources"
-		}
-	],
-	"settings": {}
-}
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("/Users/cristianedias/Desktop/Resources/election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # To do: read and analyze the data here.
+
+# Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+
+    # Print the header row.
+    headers = next(file_reader)
+    print(headers)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        print(row)
+
